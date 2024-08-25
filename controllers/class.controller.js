051f9aee7c7879774805da87f2ad5a798082d768
Anish Das <anishd988@gmail.com>
2024-08-25 19:02:32 +0530

    updates

--- controllers/class.controller.js
+++ controllers/class.controller.js
@@ -5,14 +5,12 @@ import bcrypt from 'bcrypt';
 
 
 export const genrateOtp = async (req,res)=>{
-    const {teacherId,studentId} = req.body;
+    const {teacherId,studentEmail} = req.body;
     const teacherDetails = await teacher.findById(teacherId,{classOtp:1});
-    const studentDetails = await student.findById(studentId,{email:1});
     const otp = Math.floor(100000 + Math.random() * 900000);
     const hashedOtp = await bcrypt.hash(otp.toString(),12);
-    const date = new Date();
     teacherDetails.classOtp = hashedOtp;
-    await teacher.findByIdAndUpdate(teacherId,{classOtp:{otp:hashedOtp,date}});
+    await teacher.findByIdAndUpdate(teacherId,{classOtp:{otp:hashedOtp,date:new Date()}});
     try{
         const transporter = nodemailer.createTransport({
             service:"gmail",
@@ -23,7 +21,7 @@ export const genrateOtp = async (req,res)=>{
         })
         const mailOptions = {
             from:process.env.EMAIL,
-            to:studentDetails.email,
+            to:studentEmail,
             subject:"OTP for class",
             text:`Your OTP is ${otp}`
         }
@@ -31,14 +29,11 @@ export const genrateOtp = async (req,res)=>{
             if(error){
                 console.log(error);
             }else{
-                console.log(`Email sent: ${info.response}`);
-                console.log(existingUser);
                 res.status(200).json({message:"Otp sent successfully on registered email"});
-                return;
             }
         })
     }catch(e){
-        console.error(e);
+        return res.status(500).json({message:"Something went wrong"});
     }   
 }
 
@@ -84,16 +79,30 @@ export const TakeClass = async (req,res)=>{
     const {teacherId,studentId,otp}= req.body;
     const teacherDetails = await teacher.findById(teacherId,{classOtp:1,classes:1});
     const studentDetails = await student.findById(studentId,{classes:1});
+    const otpTime = teacherDetails.classOtp.date.getTime();
+    const currentTime = new Date().getTime();
    try{
-    if(teacherDetails.classOtp.date + 900000 > new Date()){
-        const isOtpValid = bcrypt.compare(otp,teacherDetails.classOtp.otp);
+    if(otpTime+900000>currentTime){
+        const isOtpValid =await bcrypt.compare(otp,teacherDetails.classOtp.otp);
         if(isOtpValid){
-            teacherDetails.classes.classesLeft-=1;
-            studentDetails.classes.classesLeft-=1;
+            teacherDetails.classes.find((item)=>{
+                if(item.studentId==studentId){
+                    item.classesLeft = item.classesLeft-1;
+                }
+            })
+            studentDetails.classes.find((item)=>{
+                if(item.teacherId==teacherId){
+                    item.classesLeft = item.classesLeft-1;
+                }
+            })
             teacherDetails.classOtp = {otp:"",date:""};
-            if(teacherDetails.classes.classesLeft===0){
-                teacherDetails.classes = teacherDetails.classes.filter((item)=>item.studentId!==studentId);
-                studentDetails.classes = studentDetails.classes.filter((item)=>item.teacherId!==teacherId);
+            if(teacherDetails.classes.find((item)=>item.studentId==studentId && item.classesLeft<=0)){
+                teacherDetails.classes = teacherDetails.classes.filter((item)=>item.studentId!=studentId);
+                studentDetails.classes = studentDetails.classes.filter((item)=>item.teacherId!=teacherId);
+                await teacher.findByIdAndUpdate(teacherId,{classes:teacherDetails.classes,classOtp:teacherDetails.classOtp});
+                await student.findByIdAndUpdate(studentId,{classes:studentDetails.classes});
+                res.status(200).json({message:"OTP verified successfully"});
+            }else{
                 await teacher.findByIdAndUpdate(teacherId,{classes:teacherDetails.classes,classOtp:teacherDetails.classOtp});
                 await student.findByIdAndUpdate(studentId,{classes:studentDetails.classes});
                 res.status(200).json({message:"OTP verified successfully"});
